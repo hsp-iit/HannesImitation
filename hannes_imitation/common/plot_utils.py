7c194993f87ef5d42b9826381f13db5acef98212
Carlo <carlo.alessi@iit.it>
2025-01-07 14:43:31 +0100

    added utility for making plots and removing Type3 fonts

---
+++ hannes_imitation/common/plot_utils.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+FONT_SIZE   = 16
+TICK_SIZE   = 16
+LEGEND_SIZE = 12
+AXES_SIZE   = 16
+
+# change font sizes
+plt.rc('font',   size=FONT_SIZE)           # controls default text sizes
+plt.rc('axes',   labelsize=AXES_SIZE)      # font size of the x and y labels
+plt.rc('xtick',  labelsize=TICK_SIZE)      # font size of the tick labels
+plt.rc('ytick',  labelsize=TICK_SIZE)      # font size of the tick labels
+plt.rc('legend', fontsize=LEGEND_SIZE)     # legend font size
+
+# removes Type 3 fonts
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
