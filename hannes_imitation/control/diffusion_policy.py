39886ceccc373b41ba8000f75805f5b31cbec393
Carlo <carlo.alessi@iit.it>
2025-03-18 16:53:20 +0100

    added rotation of eye-in-hand frames, which are upside down from the raw readings

--- hannes_imitation/control/diffusion_policy.py
+++ hannes_imitation/control/diffusion_policy.py
@@ -12,7 +12,7 @@ from hannes_imitation.external.diffusion_policy.diffusion_policy.common.pytorch_
 
 class HannesDiffusionPolicyController:
 
-    def __init__(self, hannes, policy, control_frequency, observation_horizon, action_horizon, frames_list, hannes_states_list, lock):
+    def __init__(self, hannes, policy, control_frequency, observation_horizon, action_horizon, frames_list, hannes_states_list):
         self.hannes = hannes
         self.policy = policy
         self.control_frequency = control_frequency
@@ -20,7 +20,6 @@ class HannesDiffusionPolicyController:
         self.action_horizon = action_horizon
         self.frames_list = frames_list
         self.hannes_states_list = hannes_states_list
-        self.lock = lock
 
     def get_observation(self):
         # TODO: this function could be in env wrapper
@@ -31,12 +30,13 @@ class HannesDiffusionPolicyController:
 
         while len(obs_frames_raw) != self.observation_horizon:
             # NOTE essendo self.frames_list un multiprocessing.manager.list non dovrebbe servire la lock in quanto concorrenza già gestita 
-            with self.lock:
-                obs_frames_raw = np.array(self.frames_list[-self.observation_horizon:]) # read the latest obs_horizon frames
-        
+            obs_frames_raw = self.frames_list[-self.observation_horizon:] # read the latest obs_horizon frames
+            obs_frames_raw = np.array([frame['rgb'] for frame in obs_frames_raw]) # extract rgb key
+
         # preprocess frames
         obs_frames = [resize_image(frame, scaling_factor=0.2) for frame in obs_frames_raw] # rescale as images used for training
         obs_frames = [cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) for frame in obs_frames] # convert brg of openCV to rgb
+        obs_frames = [cv2.rotate(frame, cv2.ROTATE_180) for frame in obs_frames] # rotate eye-in-hand frames
         obs_frames = np.moveaxis(obs_frames, source=-1, destination=1) # move Channel dimension before W,H
         obs_frames = obs_frames.astype(np.float32) / 255.0 # scale to 0-1
         obs_frames = np.expand_dims(obs_frames, axis=0) # include batch size in first dimension
@@ -64,15 +64,15 @@ class HannesDiffusionPolicyController:
         action_predictions = self.policy.predict_action(obs_dict) # {'action', 'action_pred}
 
         action_trajectory = action_predictions['action'].cpu().detach().numpy() # (B, Ta, Da)
-        action_trajectory = np.array(action_trajectory).reshape(self.action_horizon, -1) # remove batch dimension (Ta, Da)
+        action_trajectory = action_trajectory.reshape(self.action_horizon, -1) # remove batch dimension (Ta, Da)
 
         return action_trajectory
     
-
     def step(self, action_trajectory):
         # actuate hand with action trajectory
         for action in action_trajectory:
             self.hannes.move_hand(int(action[0]))
             self.hannes.move_wristFE(int(action[1]))
+            self.hannes.move_wristPS(int(action[2]))
             
             time.sleep(1.0 / self.control_frequency)
