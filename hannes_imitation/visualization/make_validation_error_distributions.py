99786bf16be1c49106279adeaf5f42b64c24cd5e
carlo-alessi <carlo.alessi@iit.it>
2025-11-24 14:41:42 +0100

    updated code to visualize  error distributions

--- hannes_imitation/visualization/make_validation_error_distributions.py
+++ hannes_imitation/visualization/make_validation_error_distributions.py
@@ -16,6 +16,7 @@ from hannes_imitation.common import plot_utils
 
 data = np.load('/home/calessi-iit.local/Projects/hannes-imitation/data/validation/validation_set_results.npz', allow_pickle=True)
 results_dicts = [data[key].item() for key in data.files]
+action_horizon = 4
 
 # process results
 table_grasp_errors = []
@@ -23,25 +24,52 @@ shelf_grasp_errors = []
 human_to_hannes_handover_errors = []
 
 for result_dict in results_dicts:
-    if result_dict['scenario'] == 'table':
-        table_grasp_errors.extend(result_dict['errors'])
-    if result_dict['scenario'] == 'shelf':
-        shelf_grasp_errors.extend(result_dict['errors'])
-    if result_dict['scenario'] == '-':
-        human_to_hannes_handover_errors.extend(result_dict['errors'])
+    for h in range(action_horizon):
+        errors = result_dict['errors_%d' % h] # these are diffs
 
-table_grasp_errors = np.array(table_grasp_errors)
-shelf_grasp_errors = np.array(shelf_grasp_errors)
-human_to_hannes_handover_errors = np.array(human_to_hannes_handover_errors)
+        if result_dict['scenario'] == 'table':
+            table_grasp_errors.extend(errors)
+        if result_dict['scenario'] == 'shelf':
+            shelf_grasp_errors.extend(errors)
+        if result_dict['scenario'] == '-':
+            human_to_hannes_handover_errors.extend(errors)
 
-table_grasp_errors = np.abs(table_grasp_errors)
-shelf_grasp_errors = np.abs(shelf_grasp_errors)
-human_to_hannes_handover_errors = np.abs(human_to_hannes_handover_errors)
+table_grasp_errors = np.array(table_grasp_errors) 
+shelf_grasp_errors = np.array(shelf_grasp_errors) 
+human_to_hannes_handover_errors = np.array(human_to_hannes_handover_errors) 
 
-scenario_labels = ['Table\nGrasp (#1)', 'Shelf\nGrasp (#2)', 'Human-to-Hannes\nHandover (#3)']
+table_grasp_errors = np.abs(table_grasp_errors) / 100 * 100
+shelf_grasp_errors = np.abs(shelf_grasp_errors) / 100 * 100
+human_to_hannes_handover_errors = np.abs(human_to_hannes_handover_errors) / 60 * 100
+
+scenario_labels = ['Table\nGrasp', 'Shelf\nGrasp', 'Human-to-Hannes\nHandover']
 action_labels = ['Hand O/C', 'Wrist F/E', 'Wrist P/S']
 violin_colors = ['tab:red', 'tab:green', 'navy']
 error_data = [table_grasp_errors, shelf_grasp_errors, human_to_hannes_handover_errors]
+
+# print summary
+print("===== overall error ====")
+print("N. samples: %d" % table_grasp_errors.shape[0])
+print("Overall error: %.1f +- %.1f" % (np.concatenate(error_data).mean(), np.concatenate(error_data).std()))
+print()
+
+print("===== error by scenario ====")
+for i, error in enumerate(error_data):
+    print(scenario_labels[i].replace("\n", " "), "Average error: %.1f +- %.1f" % (np.mean(error), np.std(error)))
+print()
+
+print("===== error by action ====")
+mean_error_by_action = np.concatenate(error_data).mean(axis=0)
+std_error_by_action = np.concatenate(error_data).std(axis=0)
+for i, action_label in enumerate(action_labels):
+    print("%s: %.1f +- %.1f" % (action_label, mean_error_by_action[i], std_error_by_action[i]))
+print()
+
+print("===== error by scenario by action ====")
+for i, error in enumerate(error_data):
+    print(scenario_labels[i].replace("\n", " "), np.mean(error, axis=0).round(1), "+-", np.std(error, axis=0).round(1))
+print()
+
 
  
 # Setup figure
@@ -63,7 +91,8 @@ for i, ax in enumerate(axes):
         error_data[i],
         positions=positions,
         showfliers=False,
-        widths=0.15,
+        widths=0.2,
+        capwidths=0.2,
         patch_artist=True,
         boxprops=dict(facecolor='white', color='black', linewidth=1.5),
         whiskerprops=dict(color='black', linewidth=1.5),
@@ -74,12 +103,13 @@ for i, ax in enumerate(axes):
         median.set_color(violin_colors[j])
         median.set_linewidth(1.5)
         
-    ax.grid(linewidth=0.5, linestyle='--', axis='y')
-    ax.set_xticks([])
-    ax.set_xlabel(scenario_labels[i])
+    ax.grid(linewidth=0.5, linestyle='--')
+    ax.set_xticklabels([])
+    #ax.set_xlabel(scenario_labels[i])
+    ax.set_title(scenario_labels[i], fontsize=12)
     ax.set_facecolor('lightgrey')
 
-axes[0].set_ylabel('Action error')
+axes[0].set_ylabel('Absolute action error (%)')
 
 # legends
 axes[0].plot([], [], color=violin_colors[0], label=action_labels[0])
@@ -94,10 +124,3 @@ plt.savefig(fname='../../figures/validation/validation_error_distributions.png',
 plt.savefig(fname='../../figures/validation/validation_error_distributions.svg', bbox_inches='tight', dpi=600)
 plt.show()
 
-# print summary
-for i, error in enumerate(error_data):
-    print(scenario_labels[i].replace("\n", " "))
-    print("Average error: %.1f +- %.1f" % (np.mean(error), np.std(error)))
-    print("Average error by action:", np.mean(error, axis=0).round(1))
-    print("Maximum error by action:", np.max(error, axis=0).round(1))
-    print()
